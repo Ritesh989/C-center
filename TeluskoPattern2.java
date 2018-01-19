2358eabcd975e080ab93444185a5a706355b1a83
Ritesh989 <33152905+Ritesh989@users.noreply.github.com>
2018-01-19 06:25:40 -0800

    Add files via upload

---
+++ TeluskoPattern2.java
@@ -0,0 +1,15 @@
+class TeluskoPattern2
+{
+	public static void main(String[] args)
+	{
+		for (int i=65;i<=67;i++ )
+		{
+			for (int j=65;j<=i ;j++ )
+			{
+				System.out.print((char)j+" ");
+			}
+			System.out.println("");
+		}
+	}
+
+}
---
+++ TeluskoPattern3.java
@@ -0,0 +1,28 @@
+class TeluskoPattern3 
+{
+	public static void main(String[] args) 
+	{
+		for (int i=1;i<=4 ;i++ )
+		{
+			for (int j=1;j<=4 ;j++ )
+			{
+				if (i==2||i==3)
+				{
+					if (j==1||j==4)
+					{
+						System.out.print("$");
+					}
+					else
+					{
+					System.out.print(" ");
+					}
+				}
+				else
+				{
+				System.out.print("$");
+				}
+			}
+			System.out.println("");
+		}
+	}
+}
